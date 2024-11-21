49a70e00f5e67f1e9a851d6dacd25bf0bbf43e36
Pthuange <67030173@kmitl.ac.th>
2024-11-21 06:04:21 +0000

    isneg

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+det matn
+number = iloacnoucenter a numberi
+if number < 0:
+elif number > 8:
+print("This number is negative.")
+orint This number 1S Dositive.
+ClC.
+print("This number is both positive and negative."'
+if name == "main":
